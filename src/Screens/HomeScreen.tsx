cf9c16ea41b29323d49206c9bfb8da7f256c70ee
shannonhilland <shannonhilland@gmail.com>
2024-11-26 10:14:46 -0700

    custom font imported, CommonStyles added

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -3,16 +3,24 @@ import {
     View,
     Text,
     TouchableOpacity,
+    StyleSheet
 } from 'react-native';
+import { CommonStyles } from '../utility/Styles';
 
 export default function HomeScreen({navigation} : any) : React.JSX.Element {
     return (
-        <View>
-            <Text>
-                Hello, world!
+        <View style={CommonStyles.background}>
+            <Text style={[CommonStyles.text, CommonStyles.txtPrimaryColor]}>
+                Test with common style primary color
+            </Text>
+            <Text style={[CommonStyles.text, CommonStyles.txtSecondaryColor]}>
+                Test with common style secondary color
+            </Text>
+            <Text style={[CommonStyles.text, CommonStyles.txtTertiaryColor]}>
+                Test with common style tertiary color
             </Text>
             <TouchableOpacity onPress={() => navigation.navigate('Test')}>
-                <Text>
+                <Text style={CommonStyles.txtPrimaryColor}>
                     Go to Test
                 </Text>
             </TouchableOpacity>
---
+++ src/utility/Styles.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const CommonStyles = StyleSheet.create({
+  text: {
+    fontFamily: "CaveatBrush-Regular",
+    fontSize: 48,
+  },
+  txtPrimaryColor: {
+    color: '#F9D7A1'
+  },
+  txtSecondaryColor: {
+    color: '#7BEFDD'
+  },
+  txtTertiaryColor: {
+    color: '#BC437A'
+  },
+  background: {
+    backgroundColor: '#1E1E1E'
+  }
+});
